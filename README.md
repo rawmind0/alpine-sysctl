7b2e394919ea62059218e293d171fcc2e186b626
Raul Sanchez <rawmind@gmail.com>
2017-05-11 13:21:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+[![](https://images.microbadger.com/badges/image/rawmind/alpine-sysctl.svg)](https://microbadger.com/images/rawmind/alpine-sysctl "Get your own image badge on microbadger.com")
+
+alpine-sysctl
+=================
+
+A base image to check and set hosts sysctl parameter. 
+
+## Build
+
+```
+docker build -t rawmind/alpine-sysctl:<version> .
+```
+
+## Versions
+
+- `0.1` [(Dockerfile)](https://github.com/rawmind0/alpine-sysctl/blob/0.1/Dockerfile).
+
+## Env variables
+
+- SYSCTL_KEY=""        	# Mandatory: Set sysctl key.
+- SYSCTL_VALUE=0        # Mandatory: Set sysctl key desired value.
+- KEEP_ALIVE="0"        # Set to 1 to keep container alive. (to run in k8s)
+
+## Usage
+
+This image basically, upgrade host sysctl key value if it's lower than desired.
+
+```
+docker run -t \
+  -e "SYSCTL_KEY=KEY" \
+  -e "SYSCTL_VALUE=VALUE" \
+  --privileged \
+  rawmind/alpine-sysctl:<version> .
+```
---
+++ root/start.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+KEEP_ALIVE=${KEEP_ALIVE:-"0"}
+SYSCTL_KEY=${SYSCTL_KEY:-"vm.max_map_count"}
+SYSCTL_VALUE=${SYSCTL_VALUE:-1}
+
+if [ -n "${SYSCTL_KEY}" ] && [ "${SYSCTL_VALUE}" -gt "0" ]; then
+	VALUE=$(sysctl ${SYSCTL_KEY})
+	rc=$?
+	if [ "$rc" -eq "0" ]; then
+
+		CURRENT_VALUE=$(echo ${VALUE} | tr -d '[:space:]' | cut -d= -f2)
+
+		if [ "${CURRENT_VALUE}" -lt "${SYSCTL_VALUE}" ]; then
+    			echo `date` $ME - "Updating sysctl key ${SYSCTL_KEY} from ${CURRENT_VALUE} to ${SYSCTL_VALUE} ..."
+    			sysctl -w ${SYSCTL_KEY}=${SYSCTL_VALUE}
+			CURRENT_VALUE=$(sysctl ${SYSCTL_KEY} | tr -d '[:space:]' | cut -d= -f2)
+    		else
+    			echo `date` $ME - "Current ${SYSCTL_KEY} value is higher or equal that desired value. ${CURRENT_VALUE} - ${SYSCTL_VALUE}"
+    		fi
+    		echo `date` $ME - "sysctl key ${SYSCTL_KEY} value ${CURRENT_VALUE} ..."
+	else
+		echo `date` $ME - "[Error]: Getting sysctl key ${SYSCTL_KEY}"
+		exit 1
+	fi
+else
+	echo `date` $ME - "ERROR: You need to define SYSCTL_KEY and SYSCTL_VALUE env variables...."
+	exit 1
+fi
+
+if [ "x$KEEP_ALIVE" == "x1" ]; then
+	trap "exit 0" SIGINT SIGTERM
+	while :
+	do
+		echo `date` $ME - "I'm alive"
+		sleep 600
+	done
+fi
+
+exit 0
